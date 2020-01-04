337b9db3a483ea7cb8e387caac698d0131587840
Denis <39167005+Denis-frontend@users.noreply.github.com>
2020-01-04 14:18:43 +0200

    добавил сайт

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function green(){
+  document.getElementById('css').setAttribute('href', 'css/green.css');
+}
+
+function red(){
+  document.getElementById('css').setAttribute('href', 'css/red.css');
+}
+
+function blue(){
+  document.getElementById('css').setAttribute('href', 'css/blue.css');
+}
+
+function brown(){
+  document.getElementById('css').setAttribute('href', 'css/brown.css');
+}
+
+function orange(){
+  document.getElementById('css').setAttribute('href', 'css/orange.css');
+}
